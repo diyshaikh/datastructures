98345fd0165b6a8669ed2c2fa7fa564a86773330
BhavyaBh289 <shahbhavyasp@gmail.com>
2022-10-08 16:16:05 +0530

    .

--- tut4/prefixtoinfix.c
+++ tut4/prefixtoinfix.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <malloc.h>
+#define MAX 100
  struct node
 {
-    char data;
+    char * data;
     struct node * next;
 }* stack= NULL;
-char pop(){
+char* pop(){
     struct node *next = stack->next;
-    char c = stack->data;
+    char * c = stack->data;
     // printf("%c\n",stack->data);
     stack = next;
     return c;
 }
-void push(char n){
+void push(char * n){
     struct node *next = (struct node *) malloc(sizeof(struct node));
     next->data = n;
     next->next = stack;
@@ -23,22 +24,54 @@ void display(){
     temp = stack;
     if (temp != NULL){
         while(temp != NULL){
-            printf("data is : %c\n",temp->data);
+            printf("data is : %s\n",temp->data);
             temp = temp->next;
         }
     }
 }
+void prefixtoinfix(char Infix[], char Prefix[])
+{
+    struct stack s;
+    int i, j;
+    char* e, x;
+
+    i = 0, j = 0;
+    e = Infix[i];
+    while (e != '\0')
+    {
+        if (e == '(')
+            push(&s,e);
+        else if (isdigit(e) || isalpha(e))
+        {
+            Postfix[j++] = e;
+        }
+
+        else if (e == ')')
+        {
+            x = pop(&s);
+            while (x != '(')
+            {
+                Postfix[j++] = x;
+                x = pop(&s);
+            }
+        }else{
+            x = pop(&s);
+            while (prec(x) >= prec(e))
+            {
+                Postfix[j++] = x;
+                x = pop(&s);
+            }
+            push(&s,x);
+            push(&s,e);
+        }
+        e = Infix[++i];
+    }
+
+}
 int main() {
-//   create(10);
-    char q,c[10] = "testachar";
-  push(c[4]);
-  push(c[5]);
-  push(c[6]);
-  // push(1);
-  // push(13);
-  // push(12);
-  // push(23);
-  printf("%c",pop());
-  display();
-  return 0 ;
+    char infix[MAX], Pre[MAX];
+    printf("Postfix operation is");
+    scanf("%s", &Pre);
+    prefixtoinfix(infix, Pre);
+    printf("Infix conversion is: %s", infix);
 }
